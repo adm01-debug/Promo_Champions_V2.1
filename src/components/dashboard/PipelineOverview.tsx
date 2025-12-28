6def66060f8249b64f1b2543f336b84c1519136d
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:32:31 -0300

    feat: PipelineOverview.tsx

---
+++ src/components/dashboard/PipelineOverview.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Card } from '@/components/ui/card';
+
+export const PipelineOverview: FC = () => {
+  return <Card className="p-6"><h3>Pipeline</h3></Card>;
+};
