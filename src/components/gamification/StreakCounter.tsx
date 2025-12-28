0598342a81fc1bcb3007c40c92c7b8b5e118f7fd
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:31:16 -0300

    feat: src/components/gamification/StreakCounter.tsx

---
+++ src/components/gamification/StreakCounter.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Card } from '@/components/ui/card';
+
+export const StreakCounter: FC = () => {
+  return <Card className="p-4"><span>🔥 Streak</span></Card>;
+};
