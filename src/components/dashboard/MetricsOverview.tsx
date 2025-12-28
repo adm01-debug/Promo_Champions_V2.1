1175d2e794bbf31125833b5ae31d5064fd95f3ff
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:31:15 -0300

    feat: src/components/dashboard/MetricsOverview.tsx

---
+++ src/components/dashboard/MetricsOverview.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Card } from '@/components/ui/card';
+
+export const MetricsOverview: FC = () => {
+  return <Card className="p-6"><h3>Visão Geral</h3></Card>;
+};
