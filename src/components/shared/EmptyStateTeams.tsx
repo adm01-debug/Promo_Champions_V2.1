acae0403a67edec5e1d2f4fefcf0b0c28e3e713f
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:33:11 -0300

    feat:EmptyStateTeams.tsx

---
+++ src/components/shared/EmptyStateTeams.tsx
@@ -0,0 +1,3 @@
+import{FC}from'react';
+import{Users}from'lucide-react';
+export const EmptyStateTeams:FC=()=>{return<div className="flex flex-col items-center p-8"><Users className="h-12 w-12"/><h3>Sem times</h3></div>;};
