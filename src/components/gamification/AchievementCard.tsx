210ba7aa0fdc077f714850de9e4ad03b964ee516
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:32:28 -0300

    feat: AchievementCard.tsx

---
+++ src/components/gamification/AchievementCard.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Card } from '@/components/ui/card';
+
+export const AchievementCard: FC = () => {
+  return <Card className="p-4"><span>🏆 Achievement</span></Card>;
+};
