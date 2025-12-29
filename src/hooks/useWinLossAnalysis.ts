4d0515362b573ac0f7e32f9f1461aa56b29ae56d
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:43:12 -0300

    refactor: Usar constantes em useWinLossAnalysis.ts

--- src/hooks/useWinLossAnalysis.ts
+++ src/hooks/useWinLossAnalysis.ts
@@ -1,5 +1,7 @@
 import {useQuery} from '@tanstack/react-query';
+import { CACHE_TIMES } from '@/constants';
+
 export const useWinLossAnalysis=()=>{return useQuery({queryKey:['winLoss'],queryFn:async()=>{return {won:0,lost:0};},
-    staleTime: 5 * 60 * 1000
-    gcTime: 10 * 60 * 1000,
+    staleTime: CACHE_TIMES.STALE_TIME
+    gcTime: CACHE_TIMES.GC_TIME,
   });};
