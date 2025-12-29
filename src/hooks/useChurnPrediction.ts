9717c1bcdf81d03c0612ca56fa0f66725b80f0d4
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:34:48 -0300

    perf: Adicionar gcTime a useChurnPrediction.ts

--- src/hooks/useChurnPrediction.ts
+++ src/hooks/useChurnPrediction.ts
@@ -1,4 +1,5 @@
 import {useQuery} from '@tanstack/react-query';
 export const useChurnPrediction=()=>{return useQuery({queryKey:['churn'],queryFn:async()=>{return [];},
     staleTime: 5 * 60 * 1000
+    gcTime: 10 * 60 * 1000,
   });};
