afb916720b00317de042db66fd1687c7a6773e54
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:26:32 -0300

    fix: Adicionar staleTime a useChurnPrediction.ts

--- src/hooks/useChurnPrediction.ts
+++ src/hooks/useChurnPrediction.ts
@@ -1,2 +1,4 @@
 import {useQuery} from '@tanstack/react-query';
-export const useChurnPrediction=()=>{return useQuery({queryKey:['churn'],queryFn:async()=>{return [];}});};
+export const useChurnPrediction=()=>{return useQuery({queryKey:['churn'],queryFn:async()=>{return [];},
+    staleTime: 5 * 60 * 1000
+  });};
