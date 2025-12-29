d224f48123fb762186d228933a983b0ccc2d005b
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:26:34 -0300

    fix: Adicionar staleTime a useDealVelocity.ts

--- src/hooks/useDealVelocity.ts
+++ src/hooks/useDealVelocity.ts
@@ -1,2 +1,4 @@
 import {useQuery} from '@tanstack/react-query';
-export const useDealVelocity=()=>{return useQuery({queryKey:['velocity'],queryFn:async()=>{return 0;}});};
+export const useDealVelocity=()=>{return useQuery({queryKey:['velocity'],queryFn:async()=>{return 0;},
+    staleTime: 5 * 60 * 1000
+  });};
