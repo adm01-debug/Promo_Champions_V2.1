529d926ed55d14802b80b9cdf42e7f12c0da507d
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:16:57 -0300

    fix: Adicionar staleTime a useClients.ts

--- src/hooks/useClients.ts
+++ src/hooks/useClients.ts
@@ -18,5 +18,7 @@ export const useClients = (filters?: { segment?: string }) => {
       
       return fetchWithErrorHandling(query);
     }
+  },
+    staleTime: 5 * 60 * 1000, // 5 minutos
   });
 };
