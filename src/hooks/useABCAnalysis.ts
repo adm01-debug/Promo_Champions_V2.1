fb78b2e400bc65700f8ea854246afcaec778757d
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:26:33 -0300

    fix: Adicionar staleTime a useABCAnalysis.ts

--- src/hooks/useABCAnalysis.ts
+++ src/hooks/useABCAnalysis.ts
@@ -1,2 +1,4 @@
 import {useQuery} from '@tanstack/react-query';
-export const useABCAnalysis=()=>{return useQuery({queryKey:['abc'],queryFn:async()=>{return [];}});};
+export const useABCAnalysis=()=>{return useQuery({queryKey:['abc'],queryFn:async()=>{return [];},
+    staleTime: 5 * 60 * 1000
+  });};
