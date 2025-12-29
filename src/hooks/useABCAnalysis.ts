50506069da67a8e486749aff6bcfd5d615c8e406
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:34:48 -0300

    perf: Adicionar gcTime a useABCAnalysis.ts

--- src/hooks/useABCAnalysis.ts
+++ src/hooks/useABCAnalysis.ts
@@ -1,4 +1,5 @@
 import {useQuery} from '@tanstack/react-query';
 export const useABCAnalysis=()=>{return useQuery({queryKey:['abc'],queryFn:async()=>{return [];},
     staleTime: 5 * 60 * 1000
+    gcTime: 10 * 60 * 1000,
   });};
