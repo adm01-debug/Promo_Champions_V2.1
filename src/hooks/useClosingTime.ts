fcc2e28e66207f0ef1dd53fea9cfc69b862c1537
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:34:50 -0300

    perf: Adicionar gcTime a useClosingTime.ts

--- src/hooks/useClosingTime.ts
+++ src/hooks/useClosingTime.ts
@@ -1,4 +1,5 @@
 import {useQuery} from '@tanstack/react-query';
 export const useClosingTime=()=>{return useQuery({queryKey:['closingTime'],queryFn:async()=>{return 0;},
     staleTime: 5 * 60 * 1000
+    gcTime: 10 * 60 * 1000,
   });};
