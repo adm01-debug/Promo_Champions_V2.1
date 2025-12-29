5695f497e6927632efa7bdfc5d682b548bcf7e1c
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 19:43:06 -0300

    refactor: Usar constantes em useAchievements.ts

--- src/hooks/useAchievements.ts
+++ src/hooks/useAchievements.ts
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { supabase } from '@/integrations/supabase/client';
 import { Achievement } from '@/types';
 import { fetchWithErrorHandling } from '@/utils/supabase-helpers';
+import { CACHE_TIMES } from '@/constants';
+
 
 export const useAchievements = () => {
   return useQuery<Achievement[]>({
@@ -11,7 +13,7 @@ export const useAchievements = () => {
       return fetchWithErrorHandling(query);
     }
   ,
-    staleTime: 5 * 60 * 1000
-    gcTime: 10 * 60 * 1000,
+    staleTime: CACHE_TIMES.STALE_TIME
+    gcTime: CACHE_TIMES.GC_TIME,
   });
 };
