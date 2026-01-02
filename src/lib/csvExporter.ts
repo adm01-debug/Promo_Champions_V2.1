d513395a7ac60af13e6455e6ccf100b0e21f67d7
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 08:51:27 -0300

    feat: Add CSV export utility

---
+++ src/lib/csvExporter.ts
@@ -0,0 +1,10 @@
+import Papa from 'papaparse';
+
+export function exportToCSV<T extends Record<string, any>>(data: T[], filename: string) {
+  const csv = Papa.unparse(data);
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = `${filename}.csv`;
+  link.click();
+}
