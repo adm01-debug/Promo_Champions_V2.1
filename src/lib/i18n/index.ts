c8e16b17648c3e72686e26b7ecbaadb4a2818b35
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 16:07:21 -0300

    feat: Add i18n setup with pt-BR, en-US, es-ES

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,42 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enUS from './locales/en-US.json';
+import ptBR from './locales/pt-BR.json';
+import esES from './locales/es-ES.json';
+
+const resources = {
+  'en-US': { translation: enUS },
+  'pt-BR': { translation: ptBR },
+  'es-ES': { translation: esES },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'pt-BR',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
+
+// Hook for easy translation
+export const useI18n = () => {
+  const { t, i18n } = useTranslation();
+  
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem('language', lng);
+  };
+  
+  return { t, language: i18n.language, changeLanguage };
+};
