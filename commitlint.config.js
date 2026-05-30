6e55f53a2f2088ff495a96e17300e9714d555ee0
adm01-debug <adm01@promobrindes.com.br>
2026-05-30 16:04:16 -0300

    fix: LOW - realistic lighthouse thresholds + customized commitlint scopes for project

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,29 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'docs',
+      'style',
+      'refactor',
+      'perf',
+      'test',
+      'chore',
+      'ci',
+      'build'
+    ]],
+    'scope-enum': [2, 'always', [
+      'auth',
+      'bi',
+      'crm',
+      'gamification',
+      'ui',
+      'hooks',
+      'services',
+      'db',
+      'config',
+      'deps'
+    ]]
+  }
+};
