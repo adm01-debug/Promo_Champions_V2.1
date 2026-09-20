aafed5f7019d40cd1c35f0a85213efb5767bd941
Joaquim Ataides <ti@promobrindes.com.br>
2026-09-20 08:05:47 -0300

    chore(deps): atualizar react-router para v7.18.4

--- src/pages/Index.test.tsx
+++ src/pages/Index.test.tsx
@@ -69,9 +69,7 @@ const renderWithProviders = (ui: React.ReactNode) => {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <DashboardThemeProvider>
-          <BrowserRouter
-            future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-          >
+          <BrowserRouter>
             {ui}
           </BrowserRouter>
         </DashboardThemeProvider>
